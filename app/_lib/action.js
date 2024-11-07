69461f51e585f8b1bcffef351d5bbfd56263457c
Ata molkarae <molkaraeeata@gmail.com>
2024-11-08 00:14:00 +0300

    reservationdelete

--- app/_lib/action.js
+++ app/_lib/action.js
@@ -1,7 +1,9 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
+import { getBookings } from "./data-service";
 
 export async function guestUpdate(formData) {
   const session = await auth();
@@ -15,17 +17,18 @@ export async function guestUpdate(formData) {
     throw new Error("please provide a valid ID number");
   const updatedData = { nationalID, nationality, countryFlag };
 
-
   const { data, error } = await supabase
-  .from("guests")
-  .update(updatedData)
-  .eq("id", session.user.guestId)
-  
-if (error) {
-  console.error(error);
-  throw new Error("Guest could not be updated");
+    .from("guests")
+    .update(updatedData)
+    .eq("id", session.user.guestId);
 
-  console.log(updatedData);
+  if (error) {
+    console.error(error);
+    throw new Error("Guest could not be updated");
+
+    console.log(updatedData);
+  }
+  revalidatePath("/account/profile");
 }
 
 export default async function signInAction() {
@@ -33,4 +36,25 @@ export default async function signInAction() {
 }
 export async function signOutAction() {
   await signOut({ redirectTo: "/" });
+}
+export async function deleteReservation({ BookingId }) {
+  const session = await auth();
+  if (!session) throw new Error("You must log in first");
+
+  const guestBookings = await getBookings(session.user.guestId);
+
+  const guestBookingIds = guestBookings.includes((booking) => booking.id);
+
+  if (!guestBookingIds)
+    throw new Error("You are not allowed to delete this booking");
+
+  const { error } = await supabase
+    .from("bookings")
+    .delete()
+    .eq("id", BookingId);
+
+  if (error) {
+    throw new Error("Booking could not be deleted");
+  }
+  revalidatePath("/account/reservations");
 }
